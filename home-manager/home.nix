87bc3c79b28ca119391a6abfc472a2819450d7e7
Aiden <88350376+intothebeans@users.noreply.github.com>
2024-09-02 21:12:34 -0400

    Home Manager rebuilt on 02-09-2024 09:12:34 PM

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -88,12 +88,6 @@
       SUDO_EDITOR = "vim";
     };
 
-    file = {
-      ".config/nvim" = {
-        source = ./nvim;
-        recursive = true;
-      };
-    };
     # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
     stateVersion = "24.05";
   };
